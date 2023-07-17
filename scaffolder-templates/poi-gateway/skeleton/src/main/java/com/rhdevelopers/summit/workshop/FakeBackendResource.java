87ce46ec27d07cec35ecbe8284c0b6026a357c7f
H.P. Grahsl <grahslhp@gmail.com>
2023-07-17 10:27:02 +0200

    remove summit specific naming

--- scaffolder-templates/poi-gateway/skeleton/src/main/java/com/rhdevelopers/summit/workshop/FakeBackendResource.java
+++ scaffolder-templates/poi-gateway/skeleton/src/main/java/com/rhdevelopers/summit/workshop/FakeBackendResource.java
@@ -18,30 +18,30 @@ import com.rhdevelopers.summit.workshop.BackendRegistry.Backend;
 import com.rhdevelopers.summit.workshop.BackendRegistry.Coordinates;
 
 @Path("fake/")
-public class SummitBackendResource {
+public class FakeBackendResource {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    public static final Backend SUMMIT_BACKEND_INFO = new Backend(
-        "summit-backend",
-        "Red Hat Summit POI",
+    public static final Backend FAKE_BACKEND_INFO = new Backend(
+        "fake-backend",
+        "WeAreDevs POI",
         new Coordinates(0.0, 0.0),
         4
     );
     
-    public static final List<PoiRecord> SUMMIT_POI_RECORDS = new ArrayList<>();
+    public static final List<PoiRecord> POI_RECORDS = new ArrayList<>();
 
     @PostConstruct
     void initializePoiRecords() {
-        SUMMIT_POI_RECORDS.add(new PoiRecord("Red Hat Headquarters", "Raleigh,NC,USA", List.of(35.787743, -78.644257)));
-        SUMMIT_POI_RECORDS.add(new PoiRecord("Red Hat Summit","Boston,MA,USA", List.of(42.361145, -71.057083)));
+        POI_RECORDS.add(new PoiRecord("Red Hat Headquarters", "Raleigh,NC,USA", List.of(35.787743, -78.644257)));
+        POI_RECORDS.add(new PoiRecord("Red Hat Summit","Boston,MA,USA", List.of(42.361145, -71.057083)));
     }
 
     @GET
     @Path("ws/info")
     public Response getInfo() throws IOException {
         return Response.ok(
-            OBJECT_MAPPER.writeValueAsString(SUMMIT_BACKEND_INFO),
+            OBJECT_MAPPER.writeValueAsString(FAKE_BACKEND_INFO),
             MediaType.APPLICATION_JSON
         ).build();
     }
@@ -50,7 +50,7 @@ public class SummitBackendResource {
     @Path("poi/find/all")
     public Response getAllDataPoints() throws IOException {
         return Response.ok(
-            OBJECT_MAPPER.writeValueAsString(SUMMIT_POI_RECORDS),
+            OBJECT_MAPPER.writeValueAsString(POI_RECORDS),
             MediaType.APPLICATION_JSON
         ).build();
     }
@@ -58,12 +58,12 @@ public class SummitBackendResource {
     @GET
     @Path("poi/find/{id}")
     public Response getOneDataPoint(@PathParam("id") int id) throws IOException {
-        if(id < 0 || id >= SUMMIT_POI_RECORDS.size()) {
+        if(id < 0 || id >= POI_RECORDS.size()) {
             return Response.status(Response.Status.NOT_FOUND)
                 .entity("no data point with id "+id+" found").build();
         }
         return Response.ok(
-            OBJECT_MAPPER.writeValueAsString(SUMMIT_POI_RECORDS.get(id)),
+            OBJECT_MAPPER.writeValueAsString(POI_RECORDS.get(id)),
             MediaType.APPLICATION_JSON
         ).build();
     }
@@ -74,9 +74,9 @@ public class SummitBackendResource {
     @POST
     @Path("poi/add/point")
     public Response loadCustomPoi(PoiRecord record) {
-        SUMMIT_POI_RECORDS.add(record);
+        POI_RECORDS.add(record);
         return Response.created(
-            UriBuilder.fromPath("fake/poi/find/"+(SUMMIT_POI_RECORDS.size()-1)).build()
+            UriBuilder.fromPath("fake/poi/find/"+(POI_RECORDS.size()-1)).build()
         ).entity("inserted custom data point").build();
     }
     */
@@ -87,6 +87,7 @@ public class SummitBackendResource {
     //2. You can use curl to send the actual POST request against the gateway's API endpoint like so
 
     /*
+
     curl --location 'http://localhost:8080/fake/poi/add/point/' \
     --header 'Content-Type: application/json' \
     --data '{
@@ -94,6 +95,23 @@ public class SummitBackendResource {
     "description":"example city",
     "coordinates":[45.464203,9.189982]
     }'
+
+    curl --location 'http://localhost:8080/fake/poi/add/point/' \
+    --header 'Content-Type: application/json' \
+    --data '{
+    "name":"Graz, Austria",
+    "description":"my home town",
+    "coordinates":[47.070713,15.439504]
+    }'
+
+    curl --location 'http://localhost:8080/fake/poi/add/point/' \
+    --header 'Content-Type: application/json' \
+    --data '{
+    "name":"Berlin, Germany",
+    "description":"We Are Developers 2023",
+    "coordinates":[52.520008,13.404954]
+    }'
+
     */
     
 
